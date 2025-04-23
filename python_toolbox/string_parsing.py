8b21aafdeeab09a68d141c385ea09309b5d284c9
mcneilr2 <robyn55555@gmail.com>
2025-04-23 11:00:40 -0700

    added a general index file to run off of, updated the string parsing example to be more representative

--- python_toolbox/string_parsing.py
+++ python_toolbox/string_parsing.py
@@ -1,10 +1,30 @@
+logs = [
+    "[OK] 2024-04-01T12:30 Temp: 22.15°C",
+    "[FAIL] 2024-04-01T12:31 Temp: 33.45°C",
+    "[OK] 2024-04-01T12:32 Temp: 22.10°C"
+]
+# 
+def parse_logs(logs):
+    output = {
+        'ok_count': 0,
+        'fail_count': 0,
+        'avg_temp': 00.00,
+        'max_fail_temp': 00.00
+    }
 
-# String Parsing in Python
-def parse_string(s):
-    tokens = s.split(',')
-    cleaned = [t.strip() for t in tokens]
-    return cleaned
+    averageTemp = 0.0
+    for logString in logs:
+        tokens = logString.split(" ")
+        if tokens[0] == "[OK]":
+            output['ok_count'] += 1
+        elif tokens[0] == "[FAIL]":
+            output["fail_count"] += 1
+            output["max_fail_temp"] = max(float(tokens[3].split('°')[0]), output["max_fail_temp"])
+        averageTemp += float(tokens[3].split('°')[0])
+    
 
-# Example
-data = "apple, banana, cherry"
-print(parse_string(data))
+    output["avg_temp"] = averageTemp / len(logs)
+
+    print(output)
+
+parse_logs(logs)
